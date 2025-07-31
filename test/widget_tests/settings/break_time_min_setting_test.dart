59c4b7350204b377fc032c92abd5ee6270affbed
Patrick Zedler <mail@patrickzedler.com>
2025-07-31 23:25:01 +0200

    Improve test formatting

--- test/widget_tests/settings/break_time_min_setting_test.dart
+++ test/widget_tests/settings/break_time_min_setting_test.dart
@@ -2,10 +2,9 @@ import 'package:bloom_flutter/controller/bloom_controller.dart';
 import 'package:bloom_flutter/model/bloom_model.dart';
 import 'package:bloom_flutter/screens/settings/widgets/settings/break_time_min_setting.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-
 
 class MockBloomController extends Mock implements BloomController {}
 
@@ -17,7 +16,9 @@ void main() {
     mockController = MockBloomController();
     testModel = BloomModel(breakTimeMin: Duration(minutes: 5));
 
-    when(() => mockController.stream).thenAnswer((_) => Stream<BloomModel>.empty());
+    when(
+      () => mockController.stream,
+    ).thenAnswer((_) => Stream<BloomModel>.empty());
     when(() => mockController.state).thenReturn(testModel);
   });
 
@@ -25,7 +26,9 @@ void main() {
     return Duration(minutes: 10);
   }
 
-  testWidgets('calls controller.setBreakTimeMin after selecting new duration', (tester) async {
+  testWidgets('calls controller.setBreakTimeMin after selecting new duration', (
+    tester,
+  ) async {
     await tester.pumpWidget(
       MaterialApp(
         home: BlocProvider<BloomController>.value(
@@ -41,6 +44,8 @@ void main() {
     await tester.tap(find.byType(BreakTimeMinSetting));
     await tester.pumpAndSettle();
 
-    verify(() => mockController.setBreakTimeMin(Duration(minutes: 10))).called(1);
+    verify(
+      () => mockController.setBreakTimeMin(Duration(minutes: 10)),
+    ).called(1);
   });
 }
