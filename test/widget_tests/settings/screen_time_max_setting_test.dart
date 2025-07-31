ab7ef8d81729c3b6d041060d8def6c8a4c224c35
Patrick Zedler <mail@patrickzedler.com>
2025-07-31 23:25:54 +0200

    Improve test formatting

--- test/widget_tests/settings/screen_time_max_setting_test.dart
+++ test/widget_tests/settings/screen_time_max_setting_test.dart
@@ -2,8 +2,8 @@ import 'package:bloom_flutter/controller/bloom_controller.dart';
 import 'package:bloom_flutter/model/bloom_model.dart';
 import 'package:bloom_flutter/screens/settings/widgets/settings/screen_time_max_setting.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockBloomController extends Mock implements BloomController {}
@@ -16,7 +16,9 @@ void main() {
     mockController = MockBloomController();
     testModel = BloomModel(screenTimeMax: Duration(minutes: 5));
 
-    when(() => mockController.stream).thenAnswer((_) => Stream<BloomModel>.empty());
+    when(
+      () => mockController.stream,
+    ).thenAnswer((_) => Stream<BloomModel>.empty());
     when(() => mockController.state).thenReturn(testModel);
   });
 
@@ -24,23 +26,27 @@ void main() {
     return Duration(minutes: 10);
   }
 
-  testWidgets('calls controller.setScreenTimeMax after selecting new duration', (tester) async {
-    await tester.pumpWidget(
-      MaterialApp(
-        home: BlocProvider<BloomController>.value(
-          value: mockController,
-          child: ScreenTimeMaxSetting(
-            model: testModel,
-            showDialogFn: fakeShowDialog,
+  testWidgets(
+    'calls controller.setScreenTimeMax after selecting new duration',
+    (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BlocProvider<BloomController>.value(
+            value: mockController,
+            child: ScreenTimeMaxSetting(
+              model: testModel,
+              showDialogFn: fakeShowDialog,
+            ),
           ),
         ),
-      ),
-    );
+      );
 
-    await tester.tap(find.byType(ScreenTimeMaxSetting));
-    await tester.pumpAndSettle();
+      await tester.tap(find.byType(ScreenTimeMaxSetting));
+      await tester.pumpAndSettle();
 
-    verify(() => mockController.setScreenTimeMax(Duration(minutes: 10))).called(1);
-
-  });
+      verify(
+        () => mockController.setScreenTimeMax(Duration(minutes: 10)),
+      ).called(1);
+    },
+  );
 }
